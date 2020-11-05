896530880257719da72d5c37c1fca70a7724a48d
Stephen <s.mark.toms@gmail.com>
2020-11-05 14:20:06 -0500

    updated to have writting to correct directories

--- module.js
+++ module.js
@@ -1,45 +1,54 @@
 // module.js
-const { resolve, join } = require('path')
-const { readdirSync } = require('fs')
+import { resolve, join } from 'path'
+import { readdirSync, statSync } from 'fs'
 
-export default function(moduleOptions) {
+export default function (moduleOptions) {
   // get all options for the module
   const options = {
     ...moduleOptions,
-    ...this.options.customCounter
+    ...this.options.pixelLoader,
   }
 
   // expose the namespace / set a default
-  if (!options.namespace) options.namespace = 'customCounter'
+  if (!options.namespace) options.namespace = 'pixelLoader'
+  if (!options.folder) options.folder = 'pixels'
   const { namespace } = options
 
   // add all of the initial plugins
   const pluginsToSync = [
-    'components/index.js',
     'store/index.js',
     'plugins/index.js',
     'debug.js',
-    'middleware/index.js'
+    'middleware/index.js',
   ]
   for (const pathString of pluginsToSync) {
     this.addPlugin({
       src: resolve(__dirname, pathString),
       fileName: join(namespace, pathString),
-      options
+      options,
     })
   }
-  
+
   // sync all of the files and folders to revelant places in the nuxt build dir (.nuxt/)
-  const foldersToSync = ['plugins/helpers', 'store/modules', 'components/lib']
+  const pixelFolder = '../' + options.folder
+  const foldersToSync = ['plugins/helpers', 'store/modules', pixelFolder]
   for (const pathString of foldersToSync) {
     const path = resolve(__dirname, pathString)
     for (const file of readdirSync(path)) {
       this.addTemplate({
         src: resolve(path, file),
         fileName: join(namespace, pathString, file),
-        options
+        options,
       })
     }
   }
+
+  // configure webpack to recognize .pixel files
+  this.extendBuild((config, { isClient, isServer }) => {
+    config.module.rules.push({
+      test: /\.(pixel)$/i,
+      loader: 'raw-loader',
+    })
+  })
 }
 module.exports.meta = require('./package.json')
