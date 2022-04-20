09f65eb34fe0b073df20e927ef9c017f5eabeb34
StephenMarkToms <s.mark.toms@gmail.com>
2022-04-20 10:26:56 -0400

    Updated helpers to use addtional commenting and adding the onDemand loader

--- helpers/onLoad.js
+++ helpers/onLoad.js
@@ -1,12 +1,16 @@
-// callback - the function to run after onLoad
-// delay - wait # milliseconds after onLoad
-export const onLoad = (callback, delay) => {
+/**
+ * Wrapper for loading in scripts after the dom has finished loading
+ *
+ * @param function callback function to run after load
+ * @param integer delay timeout before firing callback
+ */
+ export const onLoad = (callback, delay = 1) => {
   // missed the load event, run now
   if (document.readyState === 'complete') {
-    setTimeout(() => callback(), delay)
+    setTimeout(() => callback(), delay);
   } else {
-    window.addEventListener('load', function () {
-      setTimeout(() => callback(), delay)
-    })
+    window.addEventListener('load', () => {
+      setTimeout(() => callback(), delay);
+    });
   }
-}
+};
